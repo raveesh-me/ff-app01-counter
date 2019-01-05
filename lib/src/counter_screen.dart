4c3663a2ba66ad2d2e96c196fdbe40150a44dadf
Raveesh Agarwal <raveeshr503@gmail.com>
2019-01-05 11:06:38 +0530

    implements the counter widget, LGTM

--- lib/src/counter_screen.dart
+++ lib/src/counter_screen.dart
@@ -6,8 +6,27 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Application'),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        setState(() {
+          counter++;
+        });
+      }, child: Icon(Icons.add),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:',),
+            Text('$counter', style: Theme.of(context).textTheme.display1,)
+          ],
+        ),
+      ),
+    );
   }
 }
